6e2d59a31540517e6284497441722f113b2a2587
triveninelakuditi <42571671+triveninelakuditi@users.noreply.github.com>
2018-12-06 11:52:50 +0530

    Create 102.py

---
+++ 102.py
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main() 
+{
+	int n,flag=0,i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	
+		if((n/i==2) && (n%i==0))
+		{
+			printf("%d",i);
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	{
+		printf("%d",n);
+	}
+	}
